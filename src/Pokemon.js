60bcd5aeb6eee9e37e01c8c937443d2534760025
Sean Pathasema <spathasema@gmail.com>
2022-05-27 15:24:35 -0500

    added grid layout to pokemon page

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 // import { useNavigate } from "react-router-dom";
 import CircularProgress from "@mui/material/CircularProgress";
 import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 import PokemonNav from "./components/PokemnNav";
 import Stats from "./components/Stats";
 import Physical from "./components/Physical";
@@ -33,7 +34,7 @@ const Pokemon = () => {
   let weight = convertToKilogram(axiosPoke.weight);
   let height2 = convertHeight(axiosPoke.height);
   let gender = genderRatio(pokemonSpecies.gender_rate);
-  let api_evolution = ''
+  let api_evolution = "";
 
   useEffect(() => {
     // fetchPokemons()
@@ -78,7 +79,7 @@ const Pokemon = () => {
         console.log(data);
         setPokemonSpecies(data);
         api_evolution = data.evolution_chain.url;
-        console.log(api_evolution)
+        console.log(api_evolution);
         // setTimeout(getEvolution, 500)
         getEvolution();
       });
@@ -86,65 +87,86 @@ const Pokemon = () => {
 
   // need to rework URL
   const getEvolution = async () => {
-    await axios
-      .get(api_evolution)
-      .then(function (response) {
-        const { data } = response;
-        console.log(data);
-        // console.log(pokemonSpecies.evolution_chain.url)
-        var evoChain = [];
-        var evoData = data.chain;
+    await axios.get(api_evolution).then(function (response) {
+      const { data } = response;
+      console.log(data);
+      // console.log(pokemonSpecies.evolution_chain.url)
+      var evoChain = [];
+      var evoData = data.chain;
 
-        do {
-          var evoDetails = evoData["evolution_details"][0];
+      do {
+        var evoDetails = evoData["evolution_details"][0];
 
-          evoChain.push({
-            species_name: evoData.species.name,
-            min_level: !evoDetails ? 1 : evoDetails.min_level,
-            trigger_name: !evoDetails ? null : evoDetails.trigger.name,
-            item: !evoDetails ? null : evoDetails.item,
-          });
+        evoChain.push({
+          species_name: evoData.species.name,
+          min_level: !evoDetails ? 1 : evoDetails.min_level,
+          trigger_name: !evoDetails ? null : evoDetails.trigger.name,
+          item: !evoDetails ? null : evoDetails.item,
+        });
 
-          evoData = evoData["evolves_to"][0];
-        } while (!!evoData && evoData.hasOwnProperty("evolves_to"));
-        setEvolution(evoChain);
-      });
-      setIsLoaded(true);
+        evoData = evoData["evolves_to"][0];
+      } while (!!evoData && evoData.hasOwnProperty("evolves_to"));
+      setEvolution(evoChain);
+    });
+    setIsLoaded(true);
   };
 
   return (
     <div>
       {isLoaded ? (
         <div>
-          <PokemonNav name={changeToTitleCase(axiosPoke.name)} id={axiosPoke.id} />
+          <PokemonNav
+            name={changeToTitleCase(axiosPoke.name)}
+            id={axiosPoke.id}
+          />
           <Box
             margin={0}
             marginTop={15}
             style={{ display: "flex", justifyContent: "space-between" }}
-            sx={{ p: 2}}
+            sx={{ p: 2 }}
           >
-            <Box marginRight={2} width="50%" height='auto' style={{ backgroundColor: blueGrey[50] }} sx={{ p: 2, boxShadow: 4  }}>
-              <div
-                className={`Pokemon-img-background ${axiosPoke.types[0].type.name}`}
-              >
-                <img src={axiosPoke.sprites.other.dream_world.front_default} />
-              </div>
-              <Stats stats={axiosPoke.stats} />
-            </Box>
-            <Box marginLeft={2} width="50%" height='auto' style={{ backgroundColor: blueGrey[50] }} sx={{ p: 2, boxShadow: 4  }}>
-              <Evolution evolution={evolution}/>
-              <Physical
-                height={height2}
-                weight={weight}
-                gender={gender}
-                category={pokemonSpecies.genera[7].genus}
-                egg={pokemonSpecies.egg_groups}
-              />
-              <TypeAbility
-                types={axiosPoke.types}
-                abilities={axiosPoke.abilities}
-              />
-            </Box>
+            <Grid container spacing={3} justifyContent="center" columns={{ md: 12, lg: 6}}>
+              <Grid item xs={12} sm={10} md={8} lg={3}>
+              <Box
+                // marginRight={2}
+                // width="50%"
+                height="auto"
+                style={{ backgroundColor: blueGrey[50] }}
+                sx={{ p: 2, boxShadow: 4 }}
+               >
+                <div
+                  className={`Pokemon-img-background ${axiosPoke.types[0].type.name}`}
+                >
+                  <img
+                    src={axiosPoke.sprites.other.dream_world.front_default}
+                  />
+                </div>
+                <Stats stats={axiosPoke.stats} />
+              </Box>
+              </Grid>
+              <Grid item xs={12} sm={10} md={8} lg={3}>
+              <Box
+                // marginLeft={2}
+                // width="50%"
+                height="auto"
+                style={{ backgroundColor: blueGrey[50] }}
+                sx={{ p: 2, boxShadow: 4 }}
+               >
+                <Evolution evolution={evolution} />
+                <Physical
+                  height={height2}
+                  weight={weight}
+                  gender={gender}
+                  category={pokemonSpecies.genera[7].genus}
+                  egg={pokemonSpecies.egg_groups}
+                />
+                <TypeAbility
+                  types={axiosPoke.types}
+                  abilities={axiosPoke.abilities}
+                />
+              </Box>
+              </Grid>
+            </Grid>
           </Box>
         </div>
       ) : (

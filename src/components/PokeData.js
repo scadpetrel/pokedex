02aeaaca7345fbe62266a81a43edd00b9777a70f
Sean Pathasema <spathasema@gmail.com>
2022-05-29 15:51:39 -0500

    additional pokedata refactor

--- src/components/PokeData.js
+++ src/components/PokeData.js
@@ -5,30 +5,34 @@ import Chip from "@mui/material/Chip";
 import Grid from "@mui/material/Grid";
 import { styled } from "@mui/material/styles";
 
-const DataItems = styled(Box)(({ theme }) => ({
+const DataContainer = styled(Box)(({ theme }) => ({
   border: "1.5px solid #4eba94",
   borderRadius: "4px",
   backgroundColor: "rgba(255, 255, 255, 0.8)",
   display: "flex",
   alignItems: "center",
   flexDirection: "column",
-  "& .pokeItem": {
-    // width: "70%",
+  "& .dataItem": {
     display: "flex",
     flexDirection: "column",
     alignItems: "center",
+  },
+  "& span": {
+    padding: "0px 1rem",
+    // display: "flex",
+    // alignItems: "center",
   },
 }));
 
 const PokeData = (props) => {
   return (
     <>
-      <DataItems sx={{ p: 3, mb: 3, boxSizing: "border-box" }} width="100%">
+      <DataContainer sx={{ p: 3, mb: 3, boxSizing: "border-box" }} width="100%">
         <Typography variant="h4" mb={4} gutterBottom>
           Pokedex Data
         </Typography>
         <Grid container>
-          <Grid className="pokeItem" item xs={12}>
+          <Grid className="dataItem" item xs={12}>
             <Typography mb={1} variant="h6">
               Category
             </Typography>
@@ -37,14 +41,14 @@ const PokeData = (props) => {
             </Typography>
           </Grid>
           <Grid container mb={3}>
-            <Grid item className="pokeItem" ml="auto" pt={2} xs={4}>
+            <Grid item className="dataItem" ml="auto" pt={2} xs={4}>
               <Typography color="#ac0031" variant="subtitle2">
                 Height
               </Typography>
               <Typography variant="body1">{props.meter} / {props.ftIn}</Typography>
               
             </Grid>
-            <Grid item className="pokeItem" mr="auto" pt={2} xs={4}>
+            <Grid item className="dataItem" mr="auto" pt={2} xs={4}>
               <Typography color="#ac0031" variant="subtitle2">
                 Weight
               </Typography>
@@ -52,29 +56,33 @@ const PokeData = (props) => {
             </Grid>
           </Grid>
           <Grid container>
-            <Grid className="pokeItem" item xs={6}>
+            <Grid className="dataItem" item xs={6}>
               <Typography mb={1} variant="h6">
                 {props.types.length > 1 ? "Types" : "Type" }
               </Typography>
               <Box style={{ display: "flex", justifyContent: "center" }}>
                 {props.types.map((type) => (
+                  <span>
                   <Chip className={type.type.name} label={type.type.name} />
+                  </span>
                 ))}
               </Box>
             </Grid>
-            <Grid className="pokeItem" item xs={6}>
+            <Grid className="dataItem" item xs={6}>
               <Typography mb={1} variant="h6">
                 Abilities
               </Typography>
               <Box style={{ display: "flex", justifyContent: "center" }}>
                 {props.abilities.map((ability) => (
+                  <span>
                   <Chip label={ability.ability.name} variant="outlined"/>
+                  </span>
                 ))}
               </Box>
             </Grid>
           </Grid>
         </Grid>
-      </DataItems>
+      </DataContainer>
     </>
   );
 };

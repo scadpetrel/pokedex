6070014df0d7723509eed041ca404b93a68a1248
Sean Pathasema <spathasema@gmail.com>
2022-07-04 14:34:59 -0500

    refactorPokeData component and styles

--- src/components/PokeData.js
+++ src/components/PokeData.js
@@ -1,43 +1,17 @@
 import React from "react";
-import Box from "@mui/material/Box";
+import { alpha } from "@mui/material/styles";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 import Chip from "@mui/material/Chip";
 import Grid from "@mui/material/Grid";
-import { styled, alpha } from "@mui/material/styles";
-import { cardGradients, typeColors } from "../helpers/colorVariables";
+import { typeColors } from "../helpers/colorVariables";
+import { DataContainer } from "./PokeDataStyles";
 
-const DataContainer = styled(Box)(({ theme }) => ({
-  border: "1.5px solid #4eba94",
-  borderRadius: "4px",
-  backgroundColor: "rgba(255, 255, 255, 0.8)",
-  display: "flex",
-  alignItems: "center",
-  flexDirection: "column",
-  "& .dataItem": {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    textAlign: "center",
-  },
-  "& span": {
-    paddingBottom: ".3rem",
-  },
-  "& .MuiChip-labelMedium": {
-    fontSize: "16px",
-    padding: "14px",
-  },
-  "& .heightWeight .dataItem": {
-    [theme.breakpoints.down("sm")]: {
-      margin: "0 auto",
-    },
-  },
-}));
-
-const PokeData = (props) => {
+const PokeData = ({category, meter, ftIn, weight, pounds, types, abilities}) => {
   return (
     <>
-      <DataContainer sx={{ p: 3, mb: 3, boxSizing: "border-box" }}>
-        <Typography variant="h4" mb={4} gutterBottom>
+      <DataContainer>
+        <Typography variant="h4" gutterBottom>
           Pokedex Data
         </Typography>
         <Grid container>
@@ -46,41 +20,36 @@ const PokeData = (props) => {
               Category
             </Typography>
             <Typography variant="subtitle1">
-              <Chip label={props.category} variant="outlined" />
+              <Chip label={category} variant="outlined" />
             </Typography>
           </Grid>
-          <Grid container mb={3} className="heightWeight">
-            <Grid item className="dataItem" ml="auto" pt={2} xs={4}>
-              <Typography color="#ac0031" variant="subtitle2">
-                Height
-              </Typography>
+          <Grid container className="heightWeight">
+            <Grid item className="dataItem" xs={4}>
+              <Typography variant="subtitle2">Height</Typography>
               <Typography variant="body1">
-                {props.meter} / {props.ftIn}
+                {meter} / {ftIn}
               </Typography>
             </Grid>
-            <Grid item className="dataItem" mr="auto" pt={2} xs={4}>
-              <Typography color="#ac0031" variant="subtitle2">
-                Weight
-              </Typography>
+            <Grid item className="dataItem" xs={4}>
+              <Typography variant="subtitle2">Weight</Typography>
               <Typography variant="body1">
-                {props.weight} kg / {props.pounds}
+                {weight} kg / {pounds}
               </Typography>
             </Grid>
           </Grid>
           <Grid container>
-            <Grid className="dataItem" item xs={12} sm={6}>
-              <Typography mb={1} variant="h6">
-                {props.types.length > 1 ? "Types" : "Type"}
+            <Grid className="dataItem typeBlock" item xs={12} sm={6}>
+              <Typography variant="h6">
+                {types.length > 1 ? "Types" : "Type"}
               </Typography>
               <Box style={{ display: "flex", justifyContent: "center" }}>
-                {props.types.map((type) => (
+                {types.map((type) => (
                   <span key={`typesKey-${type.type.name}`}>
                     <Chip
                       key={type.type.name}
                       className={type.type.name}
                       label={type.type.name}
                       style={{
-                        padding: ".5rem",
                         background: alpha(`${typeColors[type.type.name]}`, 0.2),
                       }}
                     />
@@ -88,8 +57,8 @@ const PokeData = (props) => {
                 ))}
               </Box>
             </Grid>
-            <Grid className="dataItem" item xs={12} sm={6}>
-              <Typography mb={1} variant="h6">
+            <Grid className="dataItem abilitiesBlock" item xs={12} sm={6}>
+              <Typography variant="h6">
                 Abilities
               </Typography>
               <Box
@@ -100,7 +69,7 @@ const PokeData = (props) => {
                   alignItems: "center",
                 }}
               >
-                {props.abilities.map((ability) => (
+                {abilities.map((ability) => (
                   <span key={`abilitySpan-${ability.ability.name}`}>
                     <Chip
                       key={ability.ability.name}
---
+++ src/components/PokeDataStyles.js
@@ -0,0 +1,67 @@
+import { styled, alpha } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+
+export const DataContainer = styled(Box)(({ theme }) => ({
+  border: "1.5px solid #4eba94",
+  borderRadius: "4px",
+  backgroundColor: "rgba(255, 255, 255, 0.8)",
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",  
+  padding: theme.spacing(3),
+  marginBottom: theme.spacing(3),
+  boxSizing: "border-box",
+  "& h4": {
+    marginBottom: theme.spacing(4),
+  },
+  "& .dataItem": {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+  },
+  "& span": {
+    paddingBottom: ".3rem",
+  },
+  "& .MuiChip-labelMedium": {
+    fontSize: "16px",
+    padding: "14px",
+  },
+  "& .heightWeight": {
+    marginBottom: theme.spacing(3),
+    "& .dataItem": {
+      paddingTop: theme.spacing(3),
+      "&:first-of-type": {
+        marginLeft: "auto",
+      },
+      "&:last-of-type": {
+        marginRight: "auto",
+      },
+    },
+  },
+  "& .typeBlock": {
+    "& span:first-of-type": {
+      marginRight: ".4rem",
+    },
+    "& span:last-of-type": {
+      marginLeft: ".4rem",
+    },
+    "& .MuiChip-root": {
+      padding: ".5rem",
+    },
+    "& h6": {
+      marginBottom: theme.spacing(1),
+    },
+  },
+  "& .abilitiesBlock": {
+    "& h6": {
+      marginBottom: theme.spacing(1),
+    },
+  },
+  "&.dataItem": {
+    "& h6": { color: "#ac0031" },
+    [theme.breakpoints.down("sm")]: {
+      margin: "0 auto",
+    },
+  },
+}));
